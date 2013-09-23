654630f2fe184107094aeb0d1d999144c2a21580
yibin <yibin.net@qq.com>
2013-09-23 12:24:26 +0800

    update

--- index.wsgi
+++ index.wsgi
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sae
 import tornado.wsgi,tornado
 from common import BaseHandler
@@ -20,27 +19,4 @@ tornado.web.ErrorHandler = PageNotFoundHandler
 error_url = [(r".*",PageNotFoundHandler)]
 app = tornado.wsgi.WSGIApplication(blogurls+adminurls+taskurls+installurls+error_url, **settings)
 
-=======
-import sae
-import tornado.wsgi,tornado
-from common import BaseHandler
-from blog import PageNotFoundHandler
-from blog import urls as blogurls
-from admin import urls as adminurls
-from task import urls as taskurls
-from install import urls as installurls
-settings = { 
-    'debug': False,
-    'gzip': True,
-    'cookie_secret':"61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
-    "xsrf_cookies": False
-}
-
-tornado.web.ErrorHandler = PageNotFoundHandler
-
-
-error_url = [(r".*",PageNotFoundHandler)]
-app = tornado.wsgi.WSGIApplication(blogurls+adminurls+taskurls+installurls+error_url, **settings)
-
->>>>>>> 903cf25d870f2cbcd68c2b4adc2f597bf9c9405a
 application = sae.create_wsgi_app(app)
--- task.py
+++ task.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
@@ -30,36 +29,3 @@ class Sync(tornado.web.RequestHandler):
 urls = [
     (r"/task/?", Sync),
 ]
-=======
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
-
-
-import os,time,math
-import tornado.web
-
-from setting import *
-from model import db_master
-
-class Sync(tornado.web.RequestHandler):
-    def get(self):
-        act = self.get_argument('act','')
-        if act != '':
-            if act == 'comment':
-                sql = "update py_posts set commentcount = (select count(0) from py_comment where postid=py_posts.id and status = 1 and isspam = 0)"
-            elif act =='tag':
-                sql = "update py_tags set postcount = (select count(0) from py_post_tag where tagid=py_tags.id)"
-            elif act =='category':
-                sql = "update py_category set postcount = (select count(0) from py_post_category where categoryid=py_category.id)"
-            db_master._ensure_connected()
-            db_master.execute(sql)
-            self.write('Success')
-        else:
-            self.write('Failed')
-
-
-urls = [
-    (r"/task/?", Sync),
-]
->>>>>>> 903cf25d870f2cbcd68c2b4adc2f597bf9c9405a
