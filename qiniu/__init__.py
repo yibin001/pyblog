cf85dd266f2091688acb4d1642637a83b7c571fb
yibin <yibin.net@qq.com>
2013-09-30 14:18:26 +0800

    add qiniu

---
+++ qiniu/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        __init__
+# Purpose:     PyCharm Community Edition
+#
+# Author:      HJ
+#
+# Created:     2013-09-30
+# Copyright:   (c) yibin 2013
+# Licence:     
+#-------------------------------------------------------------------------------
+import os, sys, time
